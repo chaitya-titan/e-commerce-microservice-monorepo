f85d8269e2d0577cc3970d1589ec6f75ce926c4a
chaitya-titan <official.chaitya07@gmail.com>
2025-12-29 16:40:24 +0530

    faet: completed notification service with email service

---
+++ services/notification/src/main/java/com/chaitya/ecommerce/kafka/order/Customer.java
@@ -0,0 +1,9 @@
+package com.chaitya.ecommerce.kafka.order;
+
+public record Customer (
+        String id,
+        String firstname,
+        String lastname,
+        String email
+) {
+}
---
+++ services/notification/src/main/java/com/chaitya/ecommerce/notification/NotificationType.java
@@ -0,0 +1,6 @@
+package com.chaitya.ecommerce.notification;
+
+public enum NotificationType {
+    ORDER_CONFIRMATION,
+    PAYMENT_CONFIRMATION
+}
